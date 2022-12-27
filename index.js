90a00ff077db6d758dca93fcbf2fcc518bb77208
MrTartuf0 <vitaliydidyk16@gmail.com>
2022-12-27 18:41:45 +0100

    classes

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 import puppeteer from "puppeteer"
 import fs from 'fs'
 
+
+
+(async () => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	// List of all the classrooms
+	await page.goto(`https://ittterni.altervista.org//orario_itt_2022-2023_pubblico/`);
+
+  	const data = await page.evaluate(() => {
+		let classes = Array.from(document.querySelectorAll('p a.mathema'))
+
+		return classes.map(e => e.textContent)
+  	});
+
+	fs.writeFile("classes.json", JSON.stringify(data), function(err, result) {
+		if(err) console.log('error', err);
+	});
+
+	await browser.close();
+})();
+
+
 (async () => {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	await page.goto(`https://ittterni.altervista.org//orario_itt_2022-2023_pubblico/Classi/4CIA.html`);
 
   	const data = await page.evaluate(() => {
-    	let tds = Array.from(document.querySelectorAll('td.nodecWhite , td.nodecBlack'))
-    	return tds.map(e => e.textContent)
-  	});
+		let subjects = document.querySelectorAll('td.nodecWhite , td.nodecBlack')
+    	let tds = Array.from(subjects)
 
+		for(let i=0 ; i<subjects.length ; i++){
+			tds[i] = subjects[i].rowSpan + tds[i].textContent
+		}
+
+		return tds
+  	});
 	  
 	let schedule = [] 
 	  
@@ -25,10 +52,7 @@ import fs from 'fs'
 		if(err) console.log('error', err);
 	});
 		
-	console.log(data);
 	console.log(schedule);
-	console.log(data.length);
   	
 	await browser.close();
 })();
-
