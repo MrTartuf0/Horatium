57588eb1924adea46323ff58b822884424abc034
Vitaliy Didyk <79630556+MrTartuf0@users.noreply.github.com>
2023-01-13 19:36:55 +0100

    scraped everything

--- index.js
+++ index.js
@@ -1,95 +1,127 @@
 import puppeteer from "puppeteer"
 import fs from 'fs'
 
+var dir = './data';
+
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+
 
 (async () => {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	
-	// List of all the classrooms
 	// PUBLIC SCHEDULE
 	// await page.goto(`https://ittterni.altervista.org//orario_itt_2022-2023_pubblico/`);
+
 	// PRIVATE SCHEDULE
 	await page.goto(`https://ittterni.altervista.org//orario_itt_2022-2023_riservato/index.html`);
 
-  	const allClasses = await page.evaluate(() => {
-		let classes = Array.from(document.querySelectorAll('p a.mathema'))
+	const allClasses = await page.evaluate(() => {
+		classes = Array.from(document.querySelectorAll('td')[0].querySelectorAll('p a'))
 		return classes.map(e => e.textContent)
-  	});
+	});
 
-	// Subjects of each class
-	let allSchedules = {}
+	const allTeachers = await page.evaluate(() => {
+		teachers = Array.from(document.querySelectorAll('td')[1].querySelectorAll('p a'))
+		return teachers.map(e => e.textContent)
+	});
 
-	for(let i=0 ; i<allClasses.length ; i++){
-		await page.goto(`https://ittterni.altervista.org//orario_itt_2022-2023_pubblico/Classi/${allClasses[i]}.html`);
+	const allClassRooms  = await page.evaluate(() => {
+		classrooms = Array.from(document.querySelectorAll('td')[2].querySelectorAll('p a'))
+		return classrooms.map(e => e.textContent)
+	});
+
 	
-		  const data = await page.evaluate(() => {
-			// Prendi tutti gli elementi con il tag td
-			let subjects = document.querySelectorAll('td')
-			// trasformali in array
-			let tds = Array(subjects.length) 
+	let fileNames = [['Classi' , 'Docenti' , 'Aule', ],
+	[allClasses , allTeachers , allClassRooms, ],
+	['publicSchedule.json' , 'teacherSchedule.json' , 'classRoomsSchedule.json']]
 
-			// per ogni materia prendi il text context lo spazio occupato per righe e il colore
-			// bisogna aggiungere il \n dopo ogni caratteristica ,
-			// perche dopo fara lo split('\n') e trasformera la stringa in un array
-			for(let i=0 ; i<subjects.length ; i++){
-				tds[i] = 
-				subjects[i].rowSpan +
-				'\n' + 
-				subjects[i].getAttribute('color') + 
-				'\n' + 
-				subjects[i].getAttribute('bgcolor') + 
-				subjects[i].textContent
+	
+	// SCRAPER CLASSI PER OGNI CLASSE, PER OGNI DOCENTE, E PER OGNI AULA 
+	for(let k=0 ; k<3 ; k++){
+		
+		// Subjects of each class
+		let allSchedules = {}
+		
+		for(let i=0 ; i<fileNames[1][k].length ; i++){
+			await page.goto(`https://ittterni.altervista.org//orario_itt_2022-2023_riservato/${fileNames[0][k]}/${fileNames[1][k][i]}.html`);
+		
+			const data = await page.evaluate(() => {
+				// Prendi tutti gli elementi con il tag td
+				let subjects = document.querySelectorAll('td')
+				// trasformali in array
+				let tds = Array(subjects.length) 
+	
+				// per ogni materia prendi il text context lo spazio occupato per righe e il colore
+				// bisogna aggiungere il \n dopo ogni caratteristica ,
+				// perche dopo fara lo split('\n') e trasformera la stringa in un array
+				for(let i=0 ; i<subjects.length ; i++){
+					tds[i] = 
+					subjects[i].rowSpan +
+					'\n' + 
+					subjects[i].getAttribute('color') + 
+					'\n' + 
+					subjects[i].getAttribute('bgcolor') + 
+					subjects[i].textContent
+				}
+				
+				return tds
+			});
+			  
+
+			// console.log(data);
+			let schedule = [] 
+			  
+			  for(let i=0 ; i<data.length ; i++){
+				// Creo un array seperando la stringa di prima
+				// E tolgo i caratteri inutili con filter
+				let paragraph = data[i].split("\n").filter(empty => empty!='' && empty!='\u00A0')
+				// console.log(paragraph)
+	
+				let classRoom = paragraph[paragraph.length-1]
+				let classNumber = classRoom.replace(/\D/g, "")
+	
+				let objParagraph
+	
+				(paragraph.length >= 6)
+				?objParagraph = {
+					type: 'subject',
+					height: paragraph[0],
+					color: paragraph[1],
+					backgroundColor: paragraph[2],
+					subject: paragraph[3],
+					teacher: paragraph[4],
+					classRoom: classRoom,
+					// Utilizzo le regex per prendere solo il numero dalla stringa
+					classNumber: (+classNumber > 200) ? classNumber.slice(0,-1) : classNumber
+				}
+				:objParagraph = {
+					backgroundColor: null,
+					height: paragraph[0],
+					date: (paragraph.length == 4) ? paragraph[paragraph.length-1] : null 
+				}
+	
+				schedule.push(objParagraph)
+	
 			}
-			
-			return tds
-		  });
-		  
-		let schedule = [] 
-		  
-		  for(let i=0 ; i<data.length ; i++){
-			// Creo un array seperando la stringa di prima
-			// E tolgo i caratteri inutili con filter
-			let paragraph = data[i].split("\n").filter(empty => empty!='' && empty!='\u00A0')
-			console.log(paragraph)
-
-			let classRoom = paragraph[paragraph.length-1]
-			let classNumber = classRoom.replace(/\D/g, "")
-
-			let objParagraph
-
-			(paragraph.length >= 6)
-			?objParagraph = {
-				type: 'subject',
-				height: paragraph[0],
-				color: paragraph[1],
-				backgroundColor: paragraph[2],
-				subject: paragraph[3],
-				teacher: paragraph[4],
-				classRoom: classRoom,
-				// Utilizzo le regex per prendere solo il numero dalla stringa
-				classNumber: (+classNumber > 200) ? classNumber.slice(0,-1) : classNumber
-			}
-			:objParagraph = {
-				backgroundColor: null,
-				height: paragraph[0],
-				date: (paragraph.length == 4) ? paragraph[paragraph.length-1] : null 
-			}
-
-			
-			schedule.push(objParagraph)
+	
+			Object.assign(allSchedules, {[fileNames[1][k][i]]: schedule});
+		
 		}
-		Object.assign(allSchedules, {[allClasses[i]]: schedule});
+
+		fs.writeFile(`./data/${fileNames[0][k]}.json`, JSON.stringify(fileNames[1][k], null, 4), (err, result) => {
+			if(err) console.log('error', err);
+		});
+
+		fs.writeFile(`./data/${fileNames[2][k]}`, JSON.stringify(allSchedules, null, 4), (err, result) => {
+			if(err) console.log('error', err);
+		});
+
 	}
 	
-	
-	fs.writeFile("schedule.json", JSON.stringify(allSchedules, null, 4), (err, result) => {
-		if(err) console.log('error', err);
-	});
 
-	fs.writeFile("classes.json", JSON.stringify(allClasses, null, 4), (err, result) => {
-		if(err) console.log('error', err);
-	});
 
 	// Suddivisione delle classi a seconda dell'anno
 	let classesMatrix = []
@@ -102,14 +134,13 @@ import fs from 'fs'
 		}
 		classesMatrix.push(singleYear)
 	}
-
+	
+	// Esporto in JSON un array suddiviso per indice, es [0] = tutte le classi prime e cosi via
 	fs.writeFile("classesMatrix.json", JSON.stringify(classesMatrix, null, 4), (err, result) => {
 		if(err) console.log('error', err);
 	});
 
 
-
-	// console.log(allSchedules)
 	
 	await browser.close();
 })();
